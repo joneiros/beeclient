e7c03d78a9ec1174c436a6e2366db40c211f328c
jon_gander <jon.gander@proposify.com>
2020-01-03 17:38:06 -0400

    adding landing page gif

--- src/components/ConstructionNotice.vue
+++ src/components/ConstructionNotice.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="ConstructionNotice">
-    Please bee patient. The hive is still under construction.
-    Deploy Testasf
-    <a href="/#/vue_dev_links" target="_self" rel="noopener">.</a>
+    The hive is still under construction.
+    <br>
+    Please bee patient<a href="/#/vue_dev_links" target="_self" rel="noopener">.</a>
+    <br>
+    <iframe src="https://giphy.com/embed/ZBQhoZC0nqknSviPqT" width="459" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/loading-virtual-hug-ZBQhoZC0nqknSviPqT">via GIPHY</a></p>
   </div>
 </template>
 
